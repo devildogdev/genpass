47803154de3e7d60d076c59ea4d3966f17f29344
j-tew <justinrtew@gmail.com>
2024-01-10 20:03:05 -0600

    break chars out into a package

--- main.go
+++ main.go
@@ -6,13 +6,9 @@ import (
     "errors"
     "flag"
     "math/rand"
+    "github.com/j-tew/genpass/internal/chars"
 )
 
-const asciiOffset int = 33
-var asciiLower = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-var asciiUpper = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-var asciiNum = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
-var asciiSym = []string{"!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?", "@", "[", "\\", "]", "_", "`"}
 var length int
 var upper, num, sym, all bool
 
@@ -33,21 +29,21 @@ func createPassword(length int) (error, string) {
     if length <= 0 {
         return errors.New("Error: Length must be greater than 0"), ""
     }
-    buildChars := asciiLower
+    buildChars := chars.AsciiLower
     passwordChars := make([]string, length)
     if all {
-        buildChars = append(buildChars, asciiUpper...)
-        buildChars = append(buildChars, asciiNum...)
-        buildChars = append(buildChars, asciiSym...)
+        buildChars = append(buildChars, chars.AsciiUpper...)
+        buildChars = append(buildChars, chars.AsciiNum...)
+        buildChars = append(buildChars, chars.AsciiSym...)
     }
     if upper {
-        buildChars = append(buildChars, asciiUpper...)
+        buildChars = append(buildChars, chars.AsciiUpper...)
     }
     if num {
-        buildChars = append(buildChars, asciiNum...)
+        buildChars = append(buildChars, chars.AsciiNum...)
     }
     if sym {
-        buildChars = append(buildChars, asciiSym...)
+        buildChars = append(buildChars, chars.AsciiSym...)
     }
     for i := 0; i < length; i++ {
         choice := rand.Intn(len(buildChars))
